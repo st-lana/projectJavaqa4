42eb69151b4cf582940a0fb1eca18d64a178a6d3
st-lana <st.lana@hotmail.com>
2021-12-10 11:21:25 +0300

    File Main.java updated

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,18 @@
 public class Main {
-
     public static void main(String[] args) {
-
-        int currentCount = 1000;
-        int refillSumm = 2580;
+        int currentCount = 100;
+        int refillSumm = 1200;
         int bonusLim = 1000;
+        int bonus = refillSumm / 100;
 
-        if (refillSumm > bonusLim) {
-            int bonus = refillSumm /100;
-            int finalCount = currentCount + refillSumm + bonus;
+        if (refillSumm < bonusLim) {
 
-            System.out.println("Ура! Вам начислен бонус: " + bonus + " руб." +  " Текущий счет " + finalCount + " руб.");
+            bonus = 0;
 
-        } else {
-            int finalCount = currentCount + refillSumm;
-            System.out.println("Пополнение. Сумма на счету - " + finalCount + " руб.");
         }
+        int finalCount = currentCount + refillSumm + bonus;
 
+        System.out.println("Пополнение счета. Бонус "  + bonus + " руб. " + "Текущий баланс " + finalCount + " руб.");
 
-
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете превысила ли
-        // сумма пополнения порог и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
     }
 }
