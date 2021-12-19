bc49baa2443a924b7899812d46e5004382ffd75a
st-lana <st.lana@hotmail.com>
2021-12-19 16:39:08 +0300

    Main.java upd

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int currentCount = 100;
-        int refillSumm = 1200;
+        int currentCount = 0;
+        int refillSumm = 100;
         int bonusLim = 1000;
-        int bonus = refillSumm / 100;
+        int bonus;
 
-        if (refillSumm < bonusLim) {
-
+        if (refillSumm > bonusLim) {
+            bonus = refillSumm / 100;
+        } else {
             bonus = 0;
-
         }
         int finalCount = currentCount + refillSumm + bonus;
 
